067c6687cf05fc067b82f8daed57a9dda0fb84f6
DikDns <dikdnssocial@gmail.com>
2022-07-10 09:28:17 +0700

    test final api routes and redirects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+};
 
-module.exports = nextConfig
+const redirects = async () => {
+	return [
+		{
+			source: "/hello",
+			destination: "/api/hello",
+			permanent: false,
+		},
+	];
+};
+
+module.exports = {
+	nextConfig,
+	redirects,
+};
